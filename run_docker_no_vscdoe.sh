268a5c90bdde1ba9653d4889b60e7ad29e92b785
Balla Krisztián <balla.krisztian@sztaki.hu>
2024-10-13 11:28:15 +0200

    run this if you don't want to attach a vscode server to the container

---
+++ run_docker_no_vscdoe.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+#parameters from .param file
+source ./misc/.params
+
+#add all the local processes to xhost, so the container reaches the window manager --> cv2.imshow and other display tools won't work without this
+xhost + local:
+
+if [[ $( docker ps -a -f name=$container_name | wc -l ) -eq 2 ]];
+then
+    echo "Stopping it and deleting it... ";
+    docker stop $container_name;
+    docker rm $container_name;
+fi
+
+echo "Starting the container...";
+docker run \
+    --env DISPLAY=${DISPLAY} \
+    --env NVIDIA_VISIBLE_DEVICES=all \
+    --env NVIDIA_DRIVER_CAPABILITIES=all \
+    --volume /tmp/.X11-unix:/tmp/.X11-unix \
+    --volume ${src_folder}:/home/appuser/src \
+    --volume ${vscode_folder}:/home/appuser/.vscode \
+    --volume ${requirements_file}:/home/appuser/requirements.txt \
+    --network host \
+    --interactive \
+    --privileged \
+    --detach \
+    --tty \
+    --gpus all \
+    --runtime=nvidia \
+    --name $container_name \
+    $image_name:$image_tag \
+    python /home/appuser/src/train_segmentation.py \
