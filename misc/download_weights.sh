06fcdc7b83008797ca669f6da2d08e1c9a53debe
Balla Krisztián <balla.krisztian@sztaki.hu>
2024-11-05 21:58:47 +0100

    corrected path error

--- misc/download_weights.sh
+++ misc/download_weights.sh
@@ -2,7 +2,7 @@
 
 
 FILE_ID="1_KoO10yQ6FSL505-dtlh3wbWWLpAqi0P"
-target_location="/home/krisz/PointCloudSegmentation/checkpoints/PointNet2PartSeg/2024_11_01.pth"
-mkdir -p /home/krisz/PointCloudSegmentation/checkpoints/PointNet2PartSeg
+target_location="checkpoints/PointNet2PartSeg/2024_11_01.pth"
+mkdir -p checkpoints/PointNet2PartSeg
 
 wget --no-check-certificate "https://docs.google.com/uc?export=download&id=${FILE_ID}" -O "${target_location}"
