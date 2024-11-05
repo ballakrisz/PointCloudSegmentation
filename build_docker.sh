f219772e1529cac09dc5bcc7bc0bf4b5b3832c1c
Balla Krisztián <balla.krisztian@sztaki.hu>
2024-11-05 21:49:01 +0100

    added shell script do download my pretrained weights

--- build_docker.sh
+++ build_docker.sh
@@ -8,6 +8,7 @@ source ./misc/.params
 HOST_USER_GROUP_ARG=$(id -g $USER)
 VSCODE_COMMIT_HASH=$(code --version | sed -n '2p')
 echo $VSCODE_COMMIT_HASH
+./misc/download_weights.sh
 
 #build the image
 docker build \
@@ -16,4 +17,5 @@ docker build \
     --build-arg HOST_USER_GROUP_ARG=$HOST_USER_GROUP_ARG \
     --build-arg VSCODE_COMMIT_HASH=$VSCODE_COMMIT_HASH \
     --build-arg USE_VSCODE=$use_vscode \
+    --no-cache \
     .\
