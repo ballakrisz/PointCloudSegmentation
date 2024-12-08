25fb153c0f5f538533f98fe1896e41ba0823f544
Balla Krisztián <bkrisz31@gmail.com>
2024-12-08 10:02:09 +0100

    added my own model

--- misc/download_weights.sh
+++ misc/download_weights.sh
@@ -6,3 +6,9 @@ target_location="checkpoints/PointNet2PartSeg/2024_11_01.pth"
 mkdir -p checkpoints/PointNet2PartSeg
 
 wget --no-check-certificate "https://docs.google.com/uc?export=download&id=${FILE_ID}" -O "${target_location}"
+
+FILE_ID="1OoXtxHIGmmj3H4IjQpnN44g7yYznNdx9"
+target_location="checkpoints/PointCloudSegmentation/pcs_2024-12-08-03-11-51_ckpt_latest.pth"
+mkdir -p checkpoints/PointCloudSegmentation
+
+wget --no-check-certificate "https://docs.google.com/uc?export=download&id=${FILE_ID}" -O "${target_location}"
